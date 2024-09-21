7782def336044373a190a0708316384fc8f912f9
March Anthony Dela Pena <125708648+March177@users.noreply.github.com>
2024-09-21 18:38:08 +0800

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -1,20 +1,15 @@
 <?php
 
-define('DB_HOST','localhost');
-define('DB_USER','root');
-define('DB_PASS','');
-define('DB_NAME','note_taking');
+    $conn = mysqli_connect(
+        getenv("DB_HOST"),
+        getenv("DB_USERNAME"),
+        getenv("DB_PASSWORD"),
+        getenv("DB_DATABASE"),
+        getenv("DB_PORT") ? : "3306"
+    );
 
-$conn = mysqli_connect('localhost','root','','note_taking') or die(mysqli_error());
-
-// Establish database connection.
-try
-{
-$dbh = new PDO("mysql:host=".DB_HOST.";dbname=".DB_NAME,DB_USER, DB_PASS,array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES 'utf8'"));
-}
-catch (PDOException $e)
-{
-exit("Error: " . $e->getMessage());
-}
+    if (!$conn) {
+        die("Connection failed: " . mysqli_connect_error());
+    }
 
 ?>
